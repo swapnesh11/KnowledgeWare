c49798676a7fb37b109b1174edcba1a077f2c65b
swapnesh11 <swapnesh.kunjir@gmail.com>
2018-09-06 14:23:17 +0530

    added patterns program

--- src/loopAssignment/Patterns.java
+++ src/loopAssignment/Patterns.java
@@ -3,6 +3,15 @@ package loopAssignment;
 public class Patterns {
     public static void main(String[] args) {
         Patterns p = new Patterns();
-//        p.first();
+        p.firstPatternProgram();
+    }
+    public void firstPatternProgram(){
+        for(int i=1; i<=3; i++){
+            for(int j=1; j<=i; j++){
+                System.out.println(j);
+            }
+            System.out.println();
+        }
     }
 }
+
